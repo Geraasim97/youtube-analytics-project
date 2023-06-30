70ca6471df1183781d00553bbf2e12ac29e0452a
Gerasimov <geraasim97@mail.ru>
2023-06-30 20:22:22 +0300

    done

--- src/channel.py
+++ src/channel.py
@@ -20,6 +20,21 @@ class Channel:
         self.view_count = result["items"][0]["statistics"]["viewCount"]
         self.subscriber_count = result["items"][0]["statistics"]["subscriberCount"]
 
+        def __str__(self):
+            return f"{self.title} ({self.url})"
+
+        def __add__(self, other):
+            return int(self.subscriber_count) + int(other.subscriber_count)
+
+        def __sub__(self, other):
+            return int(self.subscriber_count) - int(other.subscriber_count)
+
+        def __gt__(self, other):
+            return int(self.subscriber_count) > int(other.subscriber_count)
+
+        def __ge__(self, other):
+            return int(self.subscriber_count) >= int(other.subscriber_count)
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
