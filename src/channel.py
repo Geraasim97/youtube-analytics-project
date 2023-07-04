63a5e674063571f772db86d39c6c3b7d8602e7a8
Gerasimov <geraasim97@mail.ru>
2023-07-04 12:28:16 +0300

    done

--- src/channel.py
+++ src/channel.py
@@ -2,7 +2,8 @@ import json
 import os
 from googleapiclient.discovery import build
 
-
+api_key: str = os.getenv('YOUTUBE_EXCHANGE_DATA_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 class Channel:
     """Класс для ютуб-канала"""
 
